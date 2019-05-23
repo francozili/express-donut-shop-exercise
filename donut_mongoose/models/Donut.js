081eb40fcf521f1430887b357d54a4a5e3c9df04
francis_chukwuma <francischukwuma@PSADMINs-MacBook-Air.local>
2019-05-22 23:39:22 -0400

    made changes

--- donut_mongoose/models/Donut.js
+++ donut_mongoose/models/Donut.js
@@ -1,7 +1,23 @@
 // requirements: require db/connection as 'mongoose'
-
+const mongoose = require('../db/connection.js');
 
 // create your donut schema:
+const DonutSchema = mongoose.Schema({
+    //write your schema fields here
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    img: String,
+    price: Number,
+    qty: Number
+    });
 
 
-// export the donut model with module.exports
+    let Donut = mongoose.model("Donut", DonutSchema);
+
+    // function createDonut(newDonut) {
+    //     return DonutCollection.create(newDonut);
+    //   }
+
+
+    // export the donut model with module.exports
+module.exports = Donut
